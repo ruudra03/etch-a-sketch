b4a685fe6b2643b5af021bd39ef3aa800b2d7b70
Rudra Patel <work.rudra@aol.com>
2025-02-09 03:29:09 +0000

    Create 16x16 unit size grid on index.html using JavaScript
    Use DOM methods, loops, and createElement() to make a 16 by 16 gird (append 16*16 cells) inside the container.
    With CSS classes add styling using styles.css, after setting attributes through the script for each cell within the rows.

--- index.js
+++ index.js
@@ -2,4 +2,27 @@
 console.log("JavaScript loaded.");
 
 // Selectors for index.HTML
-const pad = document.querySelector("#sketch-pad");
+const sketchPad = document.querySelector("#sketch-pad");
+
+// Add a container to the sketchPad
+let container = document.createElement("div");
+container.setAttribute("id", "container"); // set id for the container
+sketchPad.appendChild(container);
+
+const size = 16;
+for (let x = 0; x < size; x++) {
+  let row = document.createElement("div");
+  row.setAttribute("id", `row${x + 1}`);
+  row.classList.add("row");
+
+  for (let y = 1; y < size; y++) {
+    let cell = document.createElement("div");
+    cell.classList.add("cell");
+    row.appendChild(cell);
+  }
+
+  container.appendChild(row);
+}
+
+// Review the DOM
+console.log(sketchPad);

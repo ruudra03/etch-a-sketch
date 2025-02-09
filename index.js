7ff8fd937573e2deb50962aac98ef69e4fe142f7
Rudra Patel <work.rudra@aol.com>
2025-02-09 03:41:12 +0000

    Fix the second loop for cell creation and addition in script.js
    Initiate the loop from y = 0 instead of 1.
    Use setAttribute() to provide unique ids for each cell in the corresponding row.
    Addition of more comments.

--- index.js
+++ index.js
@@ -9,18 +9,27 @@ let container = document.createElement("div");
 container.setAttribute("id", "container"); // set id for the container
 sketchPad.appendChild(container);
 
+// Grid dimensions
 const size = 16;
+
+// Create grid
 for (let x = 0; x < size; x++) {
+  // Create rows
   let row = document.createElement("div");
-  row.setAttribute("id", `row${x + 1}`);
+  row.setAttribute("id", `row${x + 1}`); // provide unique row number
   row.classList.add("row");
 
-  for (let y = 1; y < size; y++) {
+  for (let y = 0; y < size; y++) {
+    // Create cells
     let cell = document.createElement("div");
+    cell.setAttribute("id", `cell${y + 1}`); // provide unique cell number
     cell.classList.add("cell");
+
+    // Add the cell to the corresponding row
     row.appendChild(cell);
   }
 
+  // Add the row to the container
   container.appendChild(row);
 }
 
